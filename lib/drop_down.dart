14df06fb353993598b6ac270e8827db2f145dd42
Nishtha <nishtha.s@gida.io>
2023-07-07 17:34:34 +0530

    made custom_drop_down

--- lib/drop_down.dart
+++ lib/drop_down.dart
@@ -1,109 +1,143 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
 
-class DropdownPage extends StatefulWidget {
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: dateTime(),
+//     );
+//   }
+// }
+class dateTime extends StatefulWidget {
+  const dateTime({super.key});
+
   @override
-  _DropdownPageState createState() => _DropdownPageState();
+  State<dateTime> createState() => _dateTimeState();
 }
 
-class _DropdownPageState extends State<DropdownPage> {
-  final List<String> options = ['Option 1', 'Option 2', 'Option 3'];
-  final TextEditingController _typeAheadController = TextEditingController();
+class _dateTimeState extends State<dateTime> {
+  List<String> dropdownOptions = ['Option 1', 'Option 2'];
+  String? selectedOption;
+  TextEditingController newOptionController = TextEditingController();
 
-  String selectedOption = '';
+  @override
+  void dispose() {
+    newOptionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Searchable Dropdown'),
+        title: Text('Dropdown with Add Field'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TypeAheadFormField(
-              textFieldConfiguration: TextFieldConfiguration(
-                controller: _typeAheadController,
-                decoration: InputDecoration(
-                  labelText: 'Select an option',
-                ),
+      body: Column(
+        children: [
+          DropdownButtonFormField<String>(
+            value: selectedOption,
+            items: dropdownOptions.map((String option) {
+              return DropdownMenuItem<String>(
+                value: option,
+                child: Text(option),
+              );
+            }).toList(),
+            onChanged: (String? value) {
+              setState(() {
+                selectedOption = value;
+              });
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: newOptionController,
+              decoration: InputDecoration(
+                labelText: 'Add New Option',
               ),
-              suggestionsCallback: (pattern) async {
-                // Implement your own logic to fetch suggestions based on the input pattern
-                // Here, we'll just filter the options list based on the pattern
-                return options.where((option) =>
-                    option.toLowerCase().contains(pattern.toLowerCase()));
-              },
-              itemBuilder: (context, suggestion) {
-                return ListTile(
-                  title: Text(suggestion),
-                );
-              },
-              onSuggestionSelected: (suggestion) {
-                setState(() {
-                  selectedOption = suggestion;
-                  _typeAheadController.text = suggestion;
-                });
-              },
-              onSaved: (value) => selectedOption = value!,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please select an option';
-                }
-                return null;
-              },
-              noItemsFoundBuilder: (context) {
-                return ListTile(
-                  title: Text('No matching options found'),
-                );
-              },
             ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('Add New Option'),
-                      content: TextField(
-                        controller: _typeAheadController,
-                        decoration: InputDecoration(
-                          labelText: 'Option',
-                        ),
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              options.add(_typeAheadController.text);
-                              selectedOption = _typeAheadController.text;
-                              _typeAheadController.clear();
-                              Navigator.pop(context);
-                            });
-                          },
-                          child: Text('Add'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            _typeAheadController.clear();
-                            Navigator.pop(context);
-                          },
-                          child: Text('Cancel'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Text('Add New Option'),
-            ),
-            SizedBox(height: 16.0),
-            Text('Selected Option: $selectedOption'),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              String newOption = newOptionController.text;
+              setState(() {
+                dropdownOptions.add(newOption);
+                selectedOption = newOption;
+                newOptionController.clear();
+              });
+            },
+            child: Text('Add'),
+          ),
+        ],
       ),
     );
   }
 }
+
+// class DropdownWithAddField extends StatefulWidget {
+//   @override
+//   _DropdownWithAddFieldState createState() => _DropdownWithAddFieldState();
+// }
+
+// class _DropdownWithAddFieldState extends State<DropdownWithAddField> {
+//   List<String> dropdownOptions = ['Option 1', 'Option 2'];
+//   String? selectedOption;
+//   TextEditingController newOptionController = TextEditingController();
+
+//   @override
+//   void dispose() {
+//     newOptionController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Dropdown with Add Field'),
+//       ),
+//       body: Column(
+//         children: [
+//           DropdownButtonFormField<String>(
+//             value: selectedOption,
+//             items: dropdownOptions.map((String option) {
+//               return DropdownMenuItem<String>(
+//                 value: option,
+//                 child: Text(option),
+//               );
+//             }).toList(),
+//             onChanged: (String? value) {
+//               setState(() {
+//                 selectedOption = value;
+//               });
+//             },
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: TextFormField(
+//               controller: newOptionController,
+//               decoration: InputDecoration(
+//                 labelText: 'Add New Option',
+//               ),
+//             ),
+//           ),
+//           ElevatedButton(
+//             onPressed: () {
+//               String newOption = newOptionController.text;
+//               setState(() {
+//                 dropdownOptions.add(newOption);
+//                 selectedOption = newOption;
+//                 newOptionController.clear();
+//               });
+//             },
+//             child: Text('Add'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -97,6 +97,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:sample_button/drop_down.dart';
 import 'package:sample_button/hori_table.dart';
 import 'package:sample_button/login.dart';
 
@@ -105,235 +106,235 @@ import 'profile1.dart';
 
 void main() {
   // runApp(const MyApp());
-  runApp(MaterialApp(home: ProfilePage(title: 'Hello')));
+  runApp(MaterialApp(home: dateTime()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-// This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bottom NavBar Demo',
-      theme: ThemeData(
-        primaryColor: const Color(0xff2F8D46),
-        splashColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-        hoverColor: Colors.transparent,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
-}
+// // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Bottom NavBar Demo',
+//       theme: ThemeData(
+//         primaryColor: const Color(0xff2F8D46),
+//         splashColor: Colors.transparent,
+//         highlightColor: Colors.transparent,
+//         hoverColor: Colors.transparent,
+//       ),
+//       debugShowCheckedModeBanner: false,
+//       home: const HomePage(),
+//     );
+//   }
+// }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+// class HomePage extends StatefulWidget {
+//   const HomePage({Key? key}) : super(key: key);
 
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
 
-class _HomePageState extends State<HomePage> {
-  int pageIndex = 0;
+// class _HomePageState extends State<HomePage> {
+//   int pageIndex = 0;
 
-  final pages = [
-    const Page1(),
-    const Page2(),
-    const Page3(),
-    const Page4(),
-  ];
+//   final pages = [
+//     const Page1(),
+//     const Page2(),
+//     const Page3(),
+//     const Page4(),
+//   ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffC4DFCB),
-      appBar: AppBar(
-        leading: Icon(
-          Icons.menu,
-          color: Theme.of(context).primaryColor,
-        ),
-        title: Text(
-          "Geeks For Geeks",
-          style: TextStyle(
-            color: Theme.of(context).primaryColor,
-            fontSize: 25,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-      ),
-      body: pages[pageIndex],
-      bottomNavigationBar: buildMyNavBar(context),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color(0xffC4DFCB),
+//       appBar: AppBar(
+//         leading: Icon(
+//           Icons.menu,
+//           color: Theme.of(context).primaryColor,
+//         ),
+//         title: Text(
+//           "Geeks For Geeks",
+//           style: TextStyle(
+//             color: Theme.of(context).primaryColor,
+//             fontSize: 25,
+//             fontWeight: FontWeight.w600,
+//           ),
+//         ),
+//         centerTitle: true,
+//         backgroundColor: Colors.white,
+//       ),
+//       body: pages[pageIndex],
+//       bottomNavigationBar: buildMyNavBar(context),
+//     );
+//   }
 
-  Container buildMyNavBar(BuildContext context) {
-    return Container(
-      height: 60,
-      decoration: BoxDecoration(
-        color: Theme.of(context).primaryColor,
+//   Container buildMyNavBar(BuildContext context) {
+//     return Container(
+//       height: 60,
+//       decoration: BoxDecoration(
+//         color: Theme.of(context).primaryColor,
 
-        // borderRadius: const BorderRadius.only(
-        //   topLeft: Radius.circular(20),
-        //   topRight: Radius.circular(20),
-        // ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-            height: 60,
-            decoration: BoxDecoration(
-              border: pageIndex == 0
-                  ? Border(
-                      top: BorderSide(
-                        //                   <--- right side
-                        color: Colors.black,
-                        width: 3.0,
-                      ),
-                    )
-                  : null,
-            ),
-            child: IconButton(
-              enableFeedback: false,
-              onPressed: () {
-                setState(() {
-                  pageIndex = 0;
-                });
-              },
-              icon: pageIndex == 0
-                  ? const Icon(
-                      Icons.home_filled,
-                      color: Colors.white,
-                      size: 35,
-                    )
-                  : const Icon(
-                      Icons.home_outlined,
-                      color: Colors.white,
-                      size: 35,
-                    ),
-            ),
-          ),
-          Container(
-            height: 60,
-            decoration: BoxDecoration(
-              border: pageIndex == 1
-                  ? Border(
-                      top: BorderSide(
-                        //                   <--- right side
-                        color: Colors.black,
-                        width: 3.0,
-                      ),
-                    )
-                  : null,
-            ),
-            child: IconButton(
-              enableFeedback: false,
-              onPressed: () {
-                BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                    child: Dialog(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0)),
-                      backgroundColor: Colors.amber,
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 200.0,
-                        height: 200.0,
-                        child: const Text('Back Drop Filter'),
-                      ),
-                    ));
-                setState(() {
-                  pageIndex = 1;
-                });
-              },
-              icon: pageIndex == 1
-                  ? const Icon(
-                      Icons.work_rounded,
-                      color: Colors.white,
-                      size: 35,
-                    )
-                  : const Icon(
-                      Icons.work_outline_outlined,
-                      color: Colors.white,
-                      size: 35,
-                    ),
-            ),
-          ),
-          Container(
-            height: 60,
-            decoration: BoxDecoration(
-              border: pageIndex == 2
-                  ? Border(
-                      top: BorderSide(
-                        //                   <--- right side
-                        color: Colors.black,
-                        width: 3.0,
-                      ),
-                    )
-                  : null,
-            ),
-            child: IconButton(
-              enableFeedback: false,
-              onPressed: () {
-                setState(() {
-                  pageIndex = 2;
-                });
-              },
-              icon: pageIndex == 2
-                  ? const Icon(
-                      Icons.widgets_rounded,
-                      color: Colors.white,
-                      size: 35,
-                    )
-                  : const Icon(
-                      Icons.widgets_outlined,
-                      color: Colors.white,
-                      size: 35,
-                    ),
-            ),
-          ),
-          Container(
-            height: 60,
-            decoration: BoxDecoration(
-              border: pageIndex == 3
-                  ? Border(
-                      top: BorderSide(
-                        //                   <--- right side
-                        color: Colors.black,
-                        width: 3.0,
-                      ),
-                    )
-                  : null,
-            ),
-            child: IconButton(
-              enableFeedback: false,
-              onPressed: () {
-                setState(() {
-                  pageIndex = 3;
-                });
-              },
-              icon: pageIndex == 3
-                  ? const Icon(
-                      Icons.person,
-                      color: Colors.white,
-                      size: 35,
-                    )
-                  : const Icon(
-                      Icons.person_outline,
-                      color: Colors.white,
-                      size: 35,
-                    ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//         // borderRadius: const BorderRadius.only(
+//         //   topLeft: Radius.circular(20),
+//         //   topRight: Radius.circular(20),
+//         // ),
+//       ),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: [
+//           Container(
+//             height: 60,
+//             decoration: BoxDecoration(
+//               border: pageIndex == 0
+//                   ? Border(
+//                       top: BorderSide(
+//                         //                   <--- right side
+//                         color: Colors.black,
+//                         width: 3.0,
+//                       ),
+//                     )
+//                   : null,
+//             ),
+//             child: IconButton(
+//               enableFeedback: false,
+//               onPressed: () {
+//                 setState(() {
+//                   pageIndex = 0;
+//                 });
+//               },
+//               icon: pageIndex == 0
+//                   ? const Icon(
+//                       Icons.home_filled,
+//                       color: Colors.white,
+//                       size: 35,
+//                     )
+//                   : const Icon(
+//                       Icons.home_outlined,
+//                       color: Colors.white,
+//                       size: 35,
+//                     ),
+//             ),
+//           ),
+//           Container(
+//             height: 60,
+//             decoration: BoxDecoration(
+//               border: pageIndex == 1
+//                   ? Border(
+//                       top: BorderSide(
+//                         //                   <--- right side
+//                         color: Colors.black,
+//                         width: 3.0,
+//                       ),
+//                     )
+//                   : null,
+//             ),
+//             child: IconButton(
+//               enableFeedback: false,
+//               onPressed: () {
+//                 BackdropFilter(
+//                     filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+//                     child: Dialog(
+//                       shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(15.0)),
+//                       backgroundColor: Colors.amber,
+//                       child: Container(
+//                         alignment: Alignment.center,
+//                         width: 200.0,
+//                         height: 200.0,
+//                         child: const Text('Back Drop Filter'),
+//                       ),
+//                     ));
+//                 setState(() {
+//                   pageIndex = 1;
+//                 });
+//               },
+//               icon: pageIndex == 1
+//                   ? const Icon(
+//                       Icons.work_rounded,
+//                       color: Colors.white,
+//                       size: 35,
+//                     )
+//                   : const Icon(
+//                       Icons.work_outline_outlined,
+//                       color: Colors.white,
+//                       size: 35,
+//                     ),
+//             ),
+//           ),
+//           Container(
+//             height: 60,
+//             decoration: BoxDecoration(
+//               border: pageIndex == 2
+//                   ? Border(
+//                       top: BorderSide(
+//                         //                   <--- right side
+//                         color: Colors.black,
+//                         width: 3.0,
+//                       ),
+//                     )
+//                   : null,
+//             ),
+//             child: IconButton(
+//               enableFeedback: false,
+//               onPressed: () {
+//                 setState(() {
+//                   pageIndex = 2;
+//                 });
+//               },
+//               icon: pageIndex == 2
+//                   ? const Icon(
+//                       Icons.widgets_rounded,
+//                       color: Colors.white,
+//                       size: 35,
+//                     )
+//                   : const Icon(
+//                       Icons.widgets_outlined,
+//                       color: Colors.white,
+//                       size: 35,
+//                     ),
+//             ),
+//           ),
+//           Container(
+//             height: 60,
+//             decoration: BoxDecoration(
+//               border: pageIndex == 3
+//                   ? Border(
+//                       top: BorderSide(
+//                         //                   <--- right side
+//                         color: Colors.black,
+//                         width: 3.0,
+//                       ),
+//                     )
+//                   : null,
+//             ),
+//             child: IconButton(
+//               enableFeedback: false,
+//               onPressed: () {
+//                 setState(() {
+//                   pageIndex = 3;
+//                 });
+//               },
+//               icon: pageIndex == 3
+//                   ? const Icon(
+//                       Icons.person,
+//                       color: Colors.white,
+//                       size: 35,
+//                     )
+//                   : const Icon(
+//                       Icons.person_outline,
+//                       color: Colors.white,
+//                       size: 35,
+//                     ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 class Page1 extends StatelessWidget {
   const Page1({Key? key}) : super(key: key);
